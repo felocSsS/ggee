eb2ec88594de9278855e1e087e44e3b2dfd965a3
felocSsS <vlad.shipilov.felocs@gmail.com>
2021-08-26 02:09:02 +0300

    5

--- LabSes1/Pages/PageFindPatient.xaml.cs
+++ LabSes1/Pages/PageFindPatient.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LabSes1.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,71 +21,101 @@ namespace LabSes1.Pages
     /// </summary>
     public partial class PageFindPatient : Page
     {
+
+        List<dynamic> RightPatients = new List<dynamic>();
+
         public PageFindPatient()
         {
             InitializeComponent();
-            tbSlovo1.Text = "Арестант";
-            tbSlovo2.Text = "Дагестан";
+
         }
 
-        private void BtnGetLevenshtein_Click(object sender, RoutedEventArgs e)
+        private void btnFindPatient_Click(object sender, RoutedEventArgs e)
         {
-            string Slovo1 = tbSlovo1.Text;
-            string Slovo2 = tbSlovo2.Text;
-
-            int levenshtein(String a, String b)
+            if (!string.IsNullOrEmpty(tbName.Text) || !string.IsNullOrEmpty(tbBirthday.Text) || 
+                !string.IsNullOrEmpty(tbEmail.Text) || !string.IsNullOrEmpty(tbNumberStrazovania.Text) || 
+                !string.IsNullOrEmpty(tbPassport.Text) || !string.IsNullOrEmpty(tbPhone.Text)) 
             {
-                if (string.IsNullOrEmpty(a))
-                {
-                    if (!string.IsNullOrEmpty(b))
-                    {
-                        return b.Length;
-                    }
-                    return 0;
-                }
-
-                if (string.IsNullOrEmpty(b))
-                {
-                    if (!string.IsNullOrEmpty(a))
-                    {
-                        return a.Length;
-                    }
-                    return 0;
-                }
-
-                int[,] mas = new int[a.Length + 1, b.Length + 1];
-                int min1 = 0;
-                int min2 = 0;
-                int min3 = 0;
-                
-                for (int i = 0; i <= mas.GetUpperBound(0); i += 1)
-                {
-                    mas[i, 0] = i;
-                }
-                for (int i = 0; i <= mas.GetUpperBound(1); i += 1)
-                {
-                    mas[0, i] = i;
-                }
-                for (int i = 1; i <= mas.GetUpperBound(0); i += 1)
-                {
-                    for (int j = 1; j <= mas.GetUpperBound(1); j += 1)
-                    {
-                        if (a[i-1] == b[j-1])
-                        {
-                            min3 = mas[i - 1, j - 1];
-                        }
-                        else
-                        {
-                            min1 = mas[i - 1, j] + 1;
-                            min2 = mas[i, j - 1] + 1;
-                            min3 = mas[i - 1, j - 1] + 1;
-                        }
-                        mas[i, j] = Math.Min(Math.Min(min1, min2), min3);
-                    }
-                }
-                return mas[mas.GetUpperBound(0), mas.GetUpperBound(1)];
+                CalculateLevenstein();
             }
-            MessageBox.Show(levenshtein(Slovo1, Slovo2).ToString()); 
+            else
+            {
+
+            }
+        }
+
+        private void CalculateLevenstein()
+        {
+            var Patients = DataBase.db.Patient.ToList();
+
+            foreach (var Patient in Patients)
+            {
+                int CurrentPatient = levenshtein(tbName.Text, Patient.Name);
+
+                string Date = Patient.Birthday.ToString().Substring(0, Patient.Birthday.ToString().Length - 8);
+
+                if (CurrentPatient <= 3 && Date == tbBirthday.Text && tbEmail.Text == Patient.Email &&
+                    Convert.ToInt32(tbNumberStrazovania.Text) == Patient.InsuranceNumber && 
+                    Convert.ToInt32(tbPassport.Text) == Patient.Passport && Convert.ToInt32(tbPhone.Text) == Patient.PhoneNumber)
+                {
+                    RightPatients.Add(Patient);
+                }
+            }
+
+            dtPatientList.ItemsSource = RightPatients;
+        }
+
+        int levenshtein(String a, String b)
+        {
+            if (string.IsNullOrEmpty(a))
+            {
+                if (!string.IsNullOrEmpty(b))
+                {
+                    return b.Length;
+                }
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(b))
+            {
+                if (!string.IsNullOrEmpty(a))
+                {
+                    return a.Length;
+                }
+                return 0;
+            }
+
+            int[,] mas = new int[a.Length + 1, b.Length + 1];
+            int min1 = 0;
+            int min2 = 0;
+            int min3 = 0;
+
+            for (int i = 0; i <= mas.GetUpperBound(0); i += 1)
+            {
+                mas[i, 0] = i;
+            }
+            for (int i = 0; i <= mas.GetUpperBound(1); i += 1)
+            {
+                mas[0, i] = i;
+            }
+            for (int i = 1; i <= mas.GetUpperBound(0); i += 1)
+            {
+                for (int j = 1; j <= mas.GetUpperBound(1); j += 1)
+                {
+                    if (a[i - 1] == b[j - 1])
+                    {
+                        min3 = mas[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        min1 = mas[i - 1, j] + 1;
+                        min2 = mas[i, j - 1] + 1;
+                        min3 = mas[i - 1, j - 1] + 1;
+                    }
+                    mas[i, j] = Math.Min(Math.Min(min1, min2), min3);
+                }
+            }
+            return mas[mas.GetUpperBound(0), mas.GetUpperBound(1)];
         }
     }
 }
